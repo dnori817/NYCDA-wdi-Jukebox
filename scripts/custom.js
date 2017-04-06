8d8f9bf9b7cbf4010c759ee26d786626305b7a51
David Noriega <dnori817@gmail.com>
2017-04-06 13:47:53 -0400

    initial commit

---
+++ scripts/custom.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+	var JukeBox = {
+		play: function() {
+		},
+		pause: function() {
+		},
+		changeSong: function(newSong) {
+		},
+	};
+
+	class Song {
+		constructor(songFile) {
+		}
+	}
+});
